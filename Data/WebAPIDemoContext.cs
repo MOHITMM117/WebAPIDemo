02ad8e884fb0f156b13139cc3e36e7a1e8697462
Mohit Mohit <MM117@atcltd.atc.org.nz>
2021-04-16 15:30:24 +1200

    Add project files.

---
+++ Data/WebAPIDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Data
+{
+    public class WebAPIDemoContext : DbContext
+    {
+        public WebAPIDemoContext (DbContextOptions<WebAPIDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIDemo.Models.TodoItems> TodoItems { get; set; }
+    }
+}
